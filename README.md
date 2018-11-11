960822d29f8dc1bea2020ababfb65b36d59cef98
Vlad Shvaydetskiy <omihaz@gmail.com>
2018-11-11 17:20:28 +0200

    Merchant sample implementation

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# merchant-sample-php
+## Token Merchant Checkout Sample: PHP
+
+This sample code shows how to enable the Token Merchant Checkout
+experience on a simple web server.
+You can learn more about the Quick Checkout flow and relevant APIs at the
+[Merchant Quick Checkout documentation](https://developer.token.io/merchant-checkout/).
+
+To run this code, you need PHP 5.5.0 or later and the [gRPC PHP extension](https://grpc.io/docs/quickstart/php.html#install-the-grpc-php-extension).
+
+To install dependencies: `composer install`
+
+To run, `composer start`
+
+This starts up a server.
+
+The first time you run the server, it creates a new Member (Token user account).
+It saves the Member's private keys in the `keys` directory.
+In subsequent runs, the server uses this ID these keys to log the Member in.
+
+The server operates in Token's Sandbox environment. This testing environment
+lets you try out UI and payment flows without moving real money.
+
+The server shows a web page at `localhost:3000`. The page has a checkout button.
+Clicking the button starts the Token merchant payment flow.
+The server handles endorsed payments by redeeming tokens.
+
+Test by going to `localhost:3000`.
+You can't get far until you create a customer member as described at the
+[Merchant Quick Checkout documentation](https://developer.token.io/merchant-checkout/).
+
+This code uses a publicly-known developer key (the devKey line in the
+initializeSDK method). This normally works, but don't be surprised if
+it's sometimes rate-limited or disabled. If your organization will do
+more Token development and doesn't already have a developer key, contact
+Token to get one.
+
+### Troubleshooting
+
+If anything goes wrong, try to clear your browser's cache before retest.
---
+++ src/routes.php
@@ -0,0 +1,166 @@
+<?php
+
+use Io\Token\Proto\Common\Account\BankAccount;
+use Io\Token\Proto\Common\Alias\Alias;
+use Io\Token\Proto\Common\Transferinstructions\TransferEndpoint;
+use Tokenio\Config\TokenCluster;
+use Tokenio\Config\TokenEnvironment;
+use Tokenio\Config\TokenIoBuilder;
+use Tokenio\Http\Request\TokenRequest;
+use Tokenio\Http\Request\TokenRequestOptions;
+use Tokenio\Http\Request\TransferTokenBuilder;
+use Tokenio\Security\UnsecuredFileSystemKeyStore;
+use Tokenio\Util\Strings;
+
+class TokenSample
+{
+    const DEVELOPER_KEY = '4qY7lqQw8NOl9gng0ZHgT4xdiDqxqoGVutuZwrUYQsI';
+
+    /**
+     * @var UnsecuredFileSystemKeyStore
+     */
+    private $keyStore;
+
+    private $keyStoreDirectory;
+    private $tokenIO;
+    private $member;
+
+    public function __construct()
+    {
+        $this->keyStoreDirectory = __DIR__ . '/../keys/';
+        $this->keyStore = new UnsecuredFileSystemKeyStore($this->keyStoreDirectory);
+
+        $this->tokenIO = $this->initializeSDK();
+        $this->member = $this->initializeMember();
+    }
+
+    private function initializeSDK()
+    {
+        $builder = new TokenIoBuilder();
+        $builder->connectTo(TokenCluster::get(TokenEnvironment::SANDBOX));
+        $builder->developerKey(self::DEVELOPER_KEY);
+        $builder->withKeyStore($this->keyStore);
+        return $builder->build();
+    }
+
+    private function initializeMember()
+    {
+        $memberId = $this->getFirstMemberId();
+        if (!empty($memberId)) {
+            return $this->loadMember($memberId);
+        } else {
+            return $this->createMember();
+        }
+    }
+
+    /**
+     * Finds the first member id in keystore
+     *
+     * @return string|null
+     */
+    private function getFirstMemberId()
+    {
+        $directory = $this->keyStoreDirectory;
+
+        if (!file_exists($directory) || !is_dir($directory)) {
+            return null;
+        }
+
+        $files = array_diff(scandir($directory), array('.', '..'));
+        foreach ($files as $file) {
+            $filePath = $directory . DIRECTORY_SEPARATOR . $file;
+            if (is_file($filePath)) {
+                return str_replace('_', ':', $file);
+            }
+        }
+
+        return null;
+    }
+
+    private function loadMember($memberId)
+    {
+        return $this->tokenIO->getMember($memberId);
+    }
+
+    private function createMember()
+    {
+        $email = 'msphp-' . Strings::generateNonce() . '+noverify@example.com';
+
+        $alias = new Alias();
+        $alias->setType(Alias\Type::EMAIL);
+        $alias->setValue($email);
+
+        return $this->tokenIO->createBusinessMember($alias);
+    }
+
+    /**
+     * @return \Tokenio\Member
+     */
+    public function getMember()
+    {
+        return $this->member;
+    }
+
+    public function generateTokenRequestUrl($data)
+    {
+        $destinationData = json_decode($data['destination'], true);
+        $sepa = new BankAccount\Sepa();
+        $sepa->setIban($destinationData['sepa']['iban']);
+
+        $destination = new BankAccount();
+        $destination->setSepa($sepa);
+
+        $amount = $data['amount'];
+        $currency = $data['currency'];
+        $description = $data['description'];
+
+        $alias = $this->member->getFirstAlias();
+
+        $tokenBuilder = new TransferTokenBuilder($this->member, $amount, $currency);
+        $tokenBuilder->setDescription($description);
+
+        $transferEndpoint = new TransferEndpoint();
+        $transferEndpoint->setAccount($destination);
+        $tokenBuilder->addDestination($transferEndpoint);
+
+        $tokenBuilder->setToAlias($alias);
+        $tokenBuilder->setToMemberId($this->member->getMemberId());
+
+        $request = TokenRequest::builder($tokenBuilder->build())
+            ->addOption(TokenRequestOptions::REDIRECT_URL, 'http://localhost:3000/redeem')
+            ->build();
+
+        $requestId = $this->member->storeTokenRequest($request);
+
+        return $this->tokenIO->generateTokenRequestUrl($requestId);
+    }
+}
+
+$app->get('/', function ($request, $response, array $args) {
+    $this->logger->info("Index.");
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->post('/transfer', function ($request, $response, array $args) {
+    $this->logger->info("Request transfer.");
+
+    $tokenIo = new TokenSample();
+    return $response->withRedirect($tokenIo->generateTokenRequestUrl($request->getParsedBody()), 302);
+});
+
+$app->get('/redeem', function ($request, $response, array $args) {
+    $this->logger->info("Request redeem.");
+
+    $tokenId = $request->getQueryParam('tokenId');
+    if (empty($tokenId)) {
+        return 'No token id found.';
+    }
+
+    $tokenIo = new TokenSample();
+    $member = $tokenIo->getMember();
+    $token = $member->getToken($tokenId);
+
+    $transfer = $member->redeemToken($token);
+
+    return 'Success! Redeemed transfer ' . $transfer->getId();
+});
